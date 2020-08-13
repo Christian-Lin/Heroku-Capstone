513ffa21c2bf14cc545161f28865647774dbcdac
Christian Lin <christianlin@my.com>
2020-08-13 16:15:10 +0100

    Setup models and endpoints

---
+++ auth.py
@@ -0,0 +1,163 @@
+import os
+import json
+from flask import request, _request_ctx_stack, abort
+from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+AUTH0_DOMAIN = 'chris-fsnd.eu.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'Heroku'
+
+# AuthError Exception
+'''
+AuthError Exception
+A standardized way to communicate auth failure modes
+'''
+
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+# Auth Header
+def get_token_auth_header():
+    # Fetches access token from auth header
+    token = request.headers.get('Authorization', None)
+    if not token:
+        raise AuthError({
+            'code': 'auth_header_not_found',
+            'description': 'Authorization header not found'
+        }, 401)
+
+    # Split token ("Bearer" and token string)
+    parts = token.split()
+
+    if parts[0].lower() != 'bearer':
+        raise AuthError({
+            'code': 'invalid_auth_header',
+            'description': 'Authorization header must start with "Bearer".'
+        }, 401)
+
+    elif len(parts) == 1:
+        raise AuthError({
+            'code': 'invalid_auth_header',
+            'description': 'Token not found.'
+        }, 401)
+
+    elif len(parts) > 2:
+        raise AuthError({
+            'code': 'invalid_auth_header',
+            'description': 'Authorization header must have bearer token.'
+        }, 401)
+
+    # If all else passes, return token
+    token = parts[1]
+    return token
+
+
+'''
+Implement check_permissions(permission, payload) method
+'''
+
+
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_token',
+            'description': 'Permissions not found'
+        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'Unauthorized',
+            'description': 'Permission not allowed'
+        }, 403)
+
+    return True
+
+
+'''
+implement verify_decode_jwt(token) method
+'''
+
+
+def verify_decode_jwt(token):
+    # Get public key from Auth0
+    json_url = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(json_url.read())
+    # Get header
+    unverified_header = jwt.get_unverified_header(token)
+
+    # Choose key
+    rsa_key = {}
+    if 'kid' not in unverified_header:
+        raise Autherror({
+            'code': 'invalid_header',
+            'description': 'Authorization malformed.'
+        }, 401)
+    # Loop to search for a match
+    for key in jwks['keys']:
+        rsa_key = {
+            'kty': key['kty'],
+            'kid': key['kid'],
+            'use': key['use'],
+            'n': key['n'],
+            'e': key['e']
+        }
+        # Stop the loop
+        break
+
+    # Verify the signature (copied from the JWT documentation)
+    if rsa_key:
+        try:
+            # Use key to validate the JWT
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            return payload
+
+        except jwt.ExpiredSignatureError:
+            raise AuthError({
+                'code': 'token_expired',
+                'description': 'Token expired.'
+            }, 401)
+
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+            }, 401)
+        except Exception:
+            raise AuthError({
+                'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+            }, 400)
+    raise AuthError({
+        'code': 'invalid_header',
+                'description': 'Unable to find the appropriate key.'
+    }, 400)
+
+
+'''
+implement @requires_auth(permission) decorator method
+'''
+
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            payload = verify_decode_jwt(token)
+            check_permissions(permission, payload)
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+    return requires_auth_decorator
--- models.py
+++ models.py
@@ -1,1 +1,78 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
 
+# App Setup
+# db_path = os.environ.get('DATABASE_URL')
+database_name = "capstone"
+db_path = "postgres://{}/{}".format('chris:admin@localhost:5432', database_name)
+db = SQLAlchemy()
+
+def setup_db(app, db_path=db_path):
+  app.config["SQLALCHEMY_DATABASE_URI"] = db_path
+  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+  db.app = app
+  db.init_app(app)
+  db.create_all()
+
+# MODELS
+###################################################
+
+class Actor(db.Model):
+  __tablename__ = 'Actor'
+
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  age = db.Column(db.Integer, nullable=False)
+  gender = db.Column(db.String, nullable=False)
+
+  def get_name(self):
+    return self.name
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+  
+  def format(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'age': self.age,
+      'gender': self.gender
+    }
+
+class Movie(db.Model):
+  __tablename__ = 'Movie'
+
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String, nullable=False)
+  release_date = db.Column(db.DateTime(), default=datetime.utcnow)
+
+  def get_title(self):
+    return self.title
+
+  def insert(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def update(self):
+    db.session.commit()
+
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+
+  def format(self):
+    return {
+      'id': self.id,
+      'title': self.title,
+      'release_date': self.release_date
+    }
